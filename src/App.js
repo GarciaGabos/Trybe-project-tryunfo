d3031f30e20bc443b5f8367f85aba5cac71639e9
Gabriel Garcia <philiasmaximus@gmail.com>
2022-07-12 22:22:47 -0300

    Feito até o req 5

--- src/App.js
+++ src/App.js
@@ -7,30 +7,56 @@ class App extends React.Component {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
 
     this.state = {
-      cardName: ' ',
-      cardDescription: ' ',
-      cardAttr1: ' ',
-      cardAttr2: ' ',
-      cardAttr3: ' ',
-      cardImage: ' ',
-      cardRare: ' ',
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: '',
       cardTrunfo: false,
       // hasTrunfo: false,
-      // isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   onInputChange({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => this.saveConditions());
+  }
+
+  saveConditions() {
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare,
+    } = this.state;
+
+    const maxNumberSum = 210;
+    const maxNumberForEach = 90;
+    if (cardName.length !== 0
+    && cardDescription.length !== 0
+    && cardImage.length !== 0
+    && cardRare.length !== 0
+    && Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxNumberSum
+    && Number(cardAttr1) <= maxNumberForEach
+    && Number(cardAttr2) <= maxNumberForEach
+    && Number(cardAttr3) <= maxNumberForEach
+    && Number(cardAttr1) >= 0
+    && Number(cardAttr2) >= 0
+    && Number(cardAttr3) >= 0) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
     const {
       cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo,
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled,
     } = this.state;
 
     return (
@@ -45,6 +71,8 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.onSaveButtonClick }
           onInputChange={ this.onInputChange }
         />
         <Card
